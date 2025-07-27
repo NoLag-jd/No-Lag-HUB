dd801c01784f170f1f892192af69181e5f2787e6
NoLag-jd <ninogwapogo@gmail.com>
2025-07-27 18:34:47 +0800

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-jd/Test/refs/heads/main/Egg-randomizer.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/DcHOf5D9PK0Z7W68/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/39Iilcd9/raw"))()
